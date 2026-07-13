ab4798e1f1a42a4d851ec69611283351d5c53cb6
hackiteam24 <hackiteam24@gmail.com>
2026-07-13 20:09:08 +0530

    Update urls.py-sarvam speech to text

--- backend/chatbots/urls.py
+++ backend/chatbots/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ChatView, ChatHistoryView, ChatDetailView
+from .views import ChatView, ChatHistoryView, ChatDetailView, SpeechToTextView
 
 urlpatterns = [
     path('', ChatView.as_view(), name='chat'),
     path('history/', ChatHistoryView.as_view(), name='chat_history'),
     path('<uuid:chat_id>/', ChatDetailView.as_view(), name='chat_detail'),
+    path('speech-to-text/', SpeechToTextView.as_view(), name='speech_to_text'),
 ]
